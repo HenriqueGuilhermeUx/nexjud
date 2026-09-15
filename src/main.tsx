af6702e77784ee91ed17928ecf4c11ebacfef172
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-09-15 17:50:01 -0300

    Mount feature-gated NexOffice launcher

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
+import NexOfficeLauncher from './components/NexOfficeLauncher.tsx'
 import './index.css'
 import App from './App.tsx'
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
+      <NexOfficeLauncher />
     </ErrorBoundary>
   </StrictMode>,
 )
